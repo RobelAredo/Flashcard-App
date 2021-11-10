84a04457c78d6313a10d64bd646dc027506512d7
Robel Aredo <ichabodvancha@yahoo.com>
2021-11-10 10:22:54 -0600

    Added README images

--- src/Layout/StudyPageComponents/StudyCard.js
+++ src/Layout/StudyPageComponents/StudyCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function StudyCard ({ card, front}) {
+export default function StudyCard ({ card, front }) {
   const side = front ? "front" : "back";
   return (
     <>
